2314707759d87d1224f5bd0e84406e11a8dd844b
manoach <hdmanoach@gmail.com>
2025-11-08 06:02:57 +0100

    Ajout des fichiers pour de l'authentification en Django

---
+++ authentification/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomUserCreationForm
+from django.contrib.auth import login,logout,authenticate
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+# Create your views here.
+def inscription(request) :
+    if request.method == "POST" :
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid() :
+            form.save()
+            return redirect('connexion')
+    else :
+        form = CustomUserCreationForm()
+    return render(request,'inscription.html', {'form': form})
+
+def connexion(request) :
+    if request.method == "POST":
+        username= request.POST['username']
+        password= request.POST['password']
+        user= authenticate(request, username=username ,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('acceuil')
+        else :
+            messages.error(request,"Nom d'utilisateur ou mots de passe incorrect")
+    return render(request,'connexion.html')
+
+@login_required
+def acceuil(request):
+    return render(request,'acceuil.html')
+
+def deconnexion(request):
+    logout(request)
+    return redirect('connexion')
